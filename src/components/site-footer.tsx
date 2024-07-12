79725b59f5cfc715c61ad7b0082dfc538ae59a63
codemitrayt <codemitrayt@gmail.com>
2024-07-12 19:40:56 +0530

    build(comp): site footer component

---
+++ src/components/site-footer.tsx
@@ -0,0 +1,34 @@
+"use client";
+import siteConfig from "@/configs/site-config";
+
+const SiteFooter = () => {
+  return (
+    <footer className="py-6 md:px-8 md:py-0 border-t">
+      <div className="container flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
+        <p className="text-balance text-center text-sm leading-loose text-foreground md:text-left">
+          Built by{" "}
+          <a
+            href={siteConfig.links.linkedin}
+            target="_blank"
+            rel="noreferrer"
+            className="font-medium underline underline-offset-4"
+          >
+            @rushi_mungse
+          </a>
+          . The source code is available on{" "}
+          <a
+            href={siteConfig.links.github}
+            target="_blank"
+            rel="noreferrer"
+            className="font-medium underline underline-offset-4"
+          >
+            GitHub
+          </a>
+          .
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default SiteFooter;
