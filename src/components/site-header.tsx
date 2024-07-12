8afe08b2b9689acb9db8354a89f1dc367361bb6a
codemitrayt <codemitrayt@gmail.com>
2024-07-12 22:31:40 +0530

    feat: add mode-toggle button

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -4,6 +4,7 @@ import { UserButton } from "@clerk/nextjs";
 
 import LogoLink from "@/components/logo-link";
 import { Button } from "@/components/ui/button";
+import ModeToggle from "./mode-toggle";
 
 const SiteHeader = async () => {
   const user = await currentUser();
@@ -11,25 +12,30 @@ const SiteHeader = async () => {
     <header className="supports-[backdrop-filter]:bg-background/60 sticky top-0 z-50 w-screen border-b backdrop-blur">
       <div className="container flex h-14 items-center justify-between mx-auto">
         <LogoLink />
-        {user ? (
-          <div className="flex items-center justify-center space-x-2">
-            <Link href="/profile" className="text-[16px]">
-              {user?.username}
-            </Link>
-            <UserButton />
-          </div>
-        ) : (
-          <div className="flex items-center justify-center space-x-2">
-            <Link href="/sign-in">
-              <Button size="sm" variant="outline">
-                Sign In
-              </Button>
-            </Link>
-            <Link href="/sign-up">
-              <Button size="sm">Sign Up</Button>
-            </Link>
-          </div>
-        )}
+        <div className="flex items-center justify-center space-x-2">
+          {user ? (
+            <>
+              <Link href="/profile" className="text-[16px]">
+                {user?.username}
+              </Link>
+              <div className="border rounded-full flex items-center justify-center">
+                <UserButton />
+              </div>
+            </>
+          ) : (
+            <>
+              <Link href="/sign-in">
+                <Button size="sm" variant="outline">
+                  Sign In
+                </Button>
+              </Link>
+              <Link href="/sign-up">
+                <Button size="sm">Sign Up</Button>
+              </Link>
+            </>
+          )}
+          <ModeToggle />
+        </div>
       </div>
     </header>
   );
