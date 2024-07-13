be27c519eea6164073ce63aff85391d43a7716a1
codemitrayt <codemitrayt@gmail.com>
2024-07-13 18:31:43 +0530

    feat: add loader

--- src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,15 @@
-import { SignUp } from "@clerk/nextjs";
+import { ClerkLoaded, ClerkLoading, SignUp } from "@clerk/nextjs";
+import { Loader2 } from "lucide-react";
 
 export default function Page() {
-  return <SignUp />;
+  return (
+    <div className="h-[calc(100vh_-120px)] flex items-center justify-center">
+      <ClerkLoading>
+        <Loader2 className="size-12 animate-spin" />
+      </ClerkLoading>
+      <ClerkLoaded>
+        <SignUp />
+      </ClerkLoaded>
+    </div>
+  );
 }
