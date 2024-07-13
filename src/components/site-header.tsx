e120954dd6943883853d6d78e050c428fb5724ed
codemitrayt <codemitrayt@gmail.com>
2024-07-13 17:20:34 +0530

    feat: add nav-items

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -3,15 +3,19 @@ import { currentUser } from "@clerk/nextjs/server";
 import { UserButton } from "@clerk/nextjs";
 
 import LogoLink from "@/components/logo-link";
+import ModeToggle from "@/components/mode-toggle";
 import { Button } from "@/components/ui/button";
-import ModeToggle from "./mode-toggle";
+import NavItems from "@/components/nav-items";
 
 const SiteHeader = async () => {
   const user = await currentUser();
   return (
     <header className="supports-[backdrop-filter]:bg-background/60 sticky top-0 z-50 w-screen border-b backdrop-blur">
       <div className="container flex h-14 items-center justify-between mx-auto">
-        <LogoLink />
+        <div className="flex items-center justify-center space-x-6">
+          <LogoLink />
+          <NavItems />
+        </div>
         <div className="flex items-center justify-center space-x-2">
           {user ? (
             <>
