4e1e61a9e9174d70f7ca9eab5780189e3f69c7b1
codemitrayt <codemitrayt@gmail.com>
2024-07-13 20:36:48 +0530

    build(comp): profile layout component

--- src/app/(profile)/profile/layout.tsx
+++ src/app/(profile)/profile/layout.tsx
@@ -1,5 +1,20 @@
+import MobileProfileSidebar from "../_components/mobile-profile-sidebar";
+import ProfileSidebar from "../_components/profile-sidebar";
+
 const ProfileLayout = ({ children }: { children: React.ReactNode }) => {
-  return <div className="h-[calc(100vh_-120px)] bg-brand-100">{children}</div>;
+  return (
+    <div className="h-[calc(100vh_-120px)] relative">
+      <div className="flex md:hidden h-14 container mx-auto">
+        <MobileProfileSidebar />
+      </div>
+      <main className="flex h-full">
+        <div className="hidden md:block h-full w-80 bg-background border-r">
+          <ProfileSidebar />
+        </div>
+        <div className="w-full">{children}</div>
+      </main>
+    </div>
+  );
 };
 
 export default ProfileLayout;
