6c553b9da94b8e68cf5bde1a1c3cf8567c6d4345
codemitrayt <codemitrayt@gmail.com>
2024-07-13 20:37:30 +0530

    fix: active link function

--- src/components/nav-items.tsx
+++ src/components/nav-items.tsx
@@ -6,23 +6,23 @@ import { cn } from "@/lib/utils";
 
 const NavItems = () => {
   const pathname = usePathname();
-  const isActiveLink = (link: string) => pathname.includes(link);
+  const isActiveLink = (link: string) => pathname === link;
 
   return (
     <div className="flex items-center md:flex-col md:justify-center md:space-x-4">
       <div
         className={cn(
           "px-4 py-2 md:p-0 w-full hover:bg-brand-400 hover:text-brand-800 md:hover:bg-transparent md:hover:text-brand-700 md:w-fit transition",
-          isActiveLink("project") && "bg-brand-400 sm:bg-transparent"
+          isActiveLink("/projects") && "bg-brand-400 sm:bg-transparent"
         )}
       >
         <Link
           href="/projects"
           className={cn(
-            isActiveLink("project") && "text-brand-900 md:text-brand-700"
+            isActiveLink("/projects") && "text-brand-900 md:text-brand-700"
           )}
         >
-          Project
+          Projects
         </Link>
       </div>
     </div>
