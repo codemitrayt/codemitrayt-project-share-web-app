e2a438880152be96cacc614c9a70c9907a656ee7
codemitrayt <codemitrayt@gmail.com>
2024-07-13 18:31:55 +0530

    feat: add profile layout

---
+++ src/app/(profile)/profile/layout.tsx
@@ -0,0 +1,5 @@
+const ProfileLayout = ({ children }: { children: React.ReactNode }) => {
+  return <div className="h-[calc(100vh_-120px)] bg-brand-100">{children}</div>;
+};
+
+export default ProfileLayout;
