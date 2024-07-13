928560c9b3d3e4b506b1fd0fb7e326474c6b5d2a
codemitrayt <codemitrayt@gmail.com>
2024-07-13 18:31:35 +0530

    feat: add loader

--- src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,21 @@
-import { SignIn } from "@clerk/nextjs";
+import { SignIn, ClerkLoaded, ClerkLoading } from "@clerk/nextjs";
+import { Loader2 } from "lucide-react";
 
 export default function Page() {
-  return <SignIn />;
+  return (
+    <div className="h-[calc(100vh_-120px)] flex-col flex items-center justify-center">
+      <ClerkLoading>
+        <Loader2 className="size-12 animate-spin" />
+      </ClerkLoading>
+      <ClerkLoaded>
+        <div className="flex items-center flex-col justify-center mb-8 space-y-1">
+          <h1 className="font-bold text-lg  text-brand-800">
+            Welcome back to Share Project
+          </h1>
+          <p className="text-sm">Start sharing project with others.</p>
+        </div>
+        <SignIn />
+      </ClerkLoaded>
+    </div>
+  );
 }
